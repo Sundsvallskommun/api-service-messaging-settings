9fe4a7052a21f3061da36f0d0b8ec599c14453a4
Joakim Schürer <joakim.schurer@sundsvall.se>
2025-09-02 08:30:12 +0200

    HYDRAN-195: Acted on PR-comment
    - Refactored builder and specification

--- src/main/java/se/sundsvall/messagingsettings/integration/db/specification/SpecificationBuilder.java
+++ src/main/java/se/sundsvall/messagingsettings/integration/db/specification/SpecificationBuilder.java
@@ -1,77 +1,21 @@
 package se.sundsvall.messagingsettings.integration.db.specification;
 
+import static java.util.Objects.nonNull;
+
 import org.springframework.data.jpa.domain.Specification;
 
 public class SpecificationBuilder<T> {
 
-	private static final String MUNICIPALITY_ID = "municipalityId";
-	private static final String DEPARTMENT_ID = "departmentId";
-	private static final String DEPARTMENT_NAME = "departmentName";
-	private static final String NAMESPACE = "namespace";
-
 	/**
-	 * Creates filter for matching municipality id if provided, else match all
+	 * Method builds an equal filter if value is not null. If value is null, method returns
+	 * an always-true predicate (meaning no filtering will be applied for sent in attribute)
 	 *
-	 * @param  value the value to compare the municipality id to
-	 * @return       a specification that compares the municipality id to the given value (or match all if value is not
-	 *               provided)
+	 * @param  attribute name that will be used in filter
+	 * @param  value     value (or null) to compare against
+	 * @return           Specification<T> matching sent in comparison
 	 */
-	public Specification<T> buildMunicipalityIdFilter(final String value) {
-		return (entity, cq, cb) -> {
-			if (value == null) {
-				return cb.and();
-			}
-			return cb.equal(entity.get(MUNICIPALITY_ID), value);
-		};
-	}
-
-	/**
-	 * Creates filter for matching department id if provided, else match all
-	 *
-	 * @param  value the value to compare department id against
-	 * @return       a specification that compares department id against given value (or match all if value is not provided)
-	 */
-	public Specification<T> buildDepartmentIdFilter(final String value) {
-		return (entity, cq, cb) -> {
-			if (value == null) {
-				return cb.and();
-			}
-
-			return cb.equal(entity.get(DEPARTMENT_ID), value.trim());
-		};
-	}
-
-	/**
-	 * Creates filter for matching department name if provided, else match all
-	 *
-	 * @param  value the value to compare department name against
-	 * @return       a specification that compares department name against given value (or match all if value is not
-	 *               provided)
-	 */
-	public Specification<T> buildDepartmentNameFilter(final String value) {
-		return (entity, cq, cb) -> {
-			if (value == null) {
-				return cb.and();
-			}
-
-			return cb.equal(entity.get(DEPARTMENT_NAME), value.trim());
-		};
-	}
-
-	/**
-	 * Creates filter for matching namespace if provided, else match all
-	 *
-	 * @param  value the value to compare namespace against
-	 * @return       a specification that compares namespace against given value (or match all if value is not provided)
-	 */
-	public Specification<T> buildNamespaceFilter(final String value) {
-		return (entity, cq, cb) -> {
-			if (value == null) {
-				return cb.and();
-			}
-
-			return cb.equal(entity.get(NAMESPACE), value.trim());
-		};
+	public Specification<T> buildEqualFilter(String attribute, Object value) {
+		return (entity, cq, cb) -> nonNull(value) ? cb.equal(entity.get(attribute), value) : cb.and();
 	}
 
 	/**
